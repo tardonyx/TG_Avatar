dc9113603abe51c7057f00596142c04975876fcb
Andrey Bibea <andreybibea@mail.ru>
2020-02-28 09:31:13 +0300

    adding config template

---
+++ config.py
@@ -0,0 +1,17 @@
+# Telegram API constants
+telegram_api_id: int = <telegram API id>
+telegram_api_hash: str = <telegram API hash>
+tg_phone: str = <phone number in international format>
+tg_pass: str = <telegram password>
+
+# Proxy data (keep it empty if not necessary)
+proxy_ip = ''
+proxy_port = 0
+proxy_pass = ''
+
+# OpenWeather API
+openweather_api_key = <OpenWeatherMap API key>
+openweather_api_url = "http://api.openweathermap.org/data/2.5/weather"
+openweather_api_cityid = 524901  # for Moscow (for other cities look at the site)
+openweather_api_image_url = "http://openweathermap.org/img/wn/{}@2x.png"
+
