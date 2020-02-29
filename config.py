c27c4cc4fab19a71810befbc24df1a8bc241e187
Tardonyx <andreybibea@mail.ru>
2020-02-29 13:29:40 +0300

    right config file

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # Telegram API constants
-telegram_api_id = 1124391
-telegram_api_hash = '659ec19fae468dcf0d2707bd4608f474'
+telegram_api_id = 0  # Telegram API id
+telegram_api_hash = "<your Telegram API hash>"
+tg_phone = "<your phone number>"
+tg_pass = "your Telegram password"
 
 # Proxy data (keep it empty if not necessary)
-proxy_ip = 'vpn.claustrophobia.com'
-proxy_port = 1337
-proxy_pass = 'phobia'
-tg_phone = "+79772904637"
-tg_pass = "ArteXxX1994ArteXxX1994"
+proxy_ip = ''
+proxy_port = 0
+proxy_pass = ''
 
 # OpenWeather API
-openweather_api_key = "3acafd9d9a74ab54bf6795da17256abf"
+openweather_api_key = "<OpenWeatherMap API key>"
 openweather_api_url = "http://api.openweathermap.org/data/2.5/weather"
 openweather_api_cityid = 524901  # for Moscow
 openweather_api_image_url = "http://openweathermap.org/img/wn/{}@2x.png"
