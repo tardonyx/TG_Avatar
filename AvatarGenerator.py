dfb3a7ed38b91ceb84fbf4578637d1d28590223c
Tardonyx <andreybibea@mail.ru>
2020-02-29 22:35:46 +0300

    Adding docstrings, annotations and comments

--- AvatarGenerator.py
+++ AvatarGenerator.py
@@ -1,34 +1,73 @@
 # -*- coding: utf-8 -*-
 
-import json
 import config
 import os
 import aiohttp
 from datetime import datetime as dt
-from typing import Union, Tuple
+from typing import Union, Tuple, Optional
 from PIL import Image, ImageDraw, ImageFont
 
 
+WEATHER_ICONS_FOLDER_NAME = "API_Icons"  # A folder name where weather icons will be collecting.
+
+
 class AvatarGenerator(object):
+    """
+    Class that generates avatar with current time and weather data if it available.
 
-    __api_token: str
-    __api_url: str
-    __image_url: str
+    Class AvatarGenerator uses OpenWeatherMap API to load weather information (specifically
+    temperature and weather icon) for your city and generate avatar image with current time,
+    current temperature and weather icon on it.
+    The essence of this class is two methods:
+        AvatarGenerator.update_weather_data() - updates the weather information and stores it.
+        AvatarGenerator.generate() - generates avatar image with actually information.
+    Note that if it impossible to load weather information avatar image will be generate
+    only with current time on it.
 
-    __last_temperature: Union[str, None] = None
-    __last_icon: Union[str, None] = None
+    Attributes:
+        api_token: OpenWeatherMap API token - necessary string for loading weather information from API.
+        api_url: OpenWeatherMap API url - necessary string for loading weather information from API.
+        image_url: OpenWeatherMap API url pattern to load weather icon. Later in this pattern will be paste
+            string with weather icon name.
+        text_color: Tuple of three ints from 0 to 255 which encodes RGB color to text on avatar image - not
+            necessary attribute. (0, 0, 0) - by default.
+        bg_color: Tuple of three ints from 0 to 255 which encodes RGB color to background of avatar image - not
+            necessary attribute. (255, 255, 255) - by default.
+    """
 
-    __text_color: Tuple[int]
-    __bg_color: Tuple[int]
+    __api_token: str    # Variable which stores OpenWeatherMap API token
+    __api_url: str      # Variable which stores OpenWeatherMap API url
+    __image_url: str    # Variable which stores OpenWeatherMap API url pattern to load weather icon
+
+    __last_temperature: Union[str, None] = None     # Last loaded temperature from OpenWeatherMap API or None
+    __last_icon: Union[str, None] = None            # Last loaded weather icon name from OpenWeatherMap API or None
+
+    __text_color: Tuple[int]    # Text color (must be a tuple of three ints from 0 to 255 - RGB format)
+    __bg_color: Tuple[int]      # Background color (must be a tuple of three ints from 0 to 255 - RGB format)
 
     def __init__(self, api_token, api_url, image_url, text_color=(0, 0, 0), bg_color=(255, 255, 255)):
+        """Inits AvatarGenerator with necessary API information and colors for text and background."""
         self.__api_token = api_token
         self.__api_url = api_url
         self.__image_url = image_url
         self.__text_color = text_color
         self.__bg_color = bg_color
 
-    async def update_weather_data(self, city_id=524901):
+    async def update_weather_data(self, city_id: int = 524901) -> Tuple[Optional[str], Optional[str]]:
+        """
+        Method which makes a GET request to OpenWeatherMap API in order to get current temperature and
+        weather icon name to your city. It overwrites stored weather data with actual weather data or
+        with None if request is unsuccessfully. Returns the tuple of updated weather data (temperature
+        and weather icon name).
+
+        Args:
+            city_id: The code of the city for which you want to receive the weather data. Code for the Moscow
+                city by default.
+
+        Returns:
+            Tuple of updated temperature and weather icon name or with Nones.
+        """
+        # Necessary information for request which loading in query string
         payload = {
             "id": city_id,
             "appid": self.__api_token,
@@ -39,29 +78,58 @@ class AvatarGenerator(object):
                 params=payload,
             ) as response:
                 if response.status == 200:
+                    # if request is success updating weather data with actual
                     data = await response.json(encoding="utf-8")
                     self.__last_temperature = data["main"]["temp"]
                     self.__last_icon = data["weather"][0]["icon"]
                     await self.__check_weather_image(self.__last_icon)
+                else:
+                    # if request is not success - updating with Nones
+                    self.__last_temperature = None
+                    self.__last_icon = None
         return self.__last_temperature, self.__last_icon
 
     @staticmethod
-    def __weather_image_exists(image_name=""):
-        img_path = os.path.join(os.getcwd(), "API_Icons", image_name+".png")
+    def __weather_image_exists(image_name: str) -> bool:
+        """Check if weather image icon is exists in folder."""
+        img_path = os.path.join(os.getcwd(), WEATHER_ICONS_FOLDER_NAME, image_name+".png")
         return os.path.exists(img_path)
 
-    async def __check_weather_image(self, image_name=""):
+    async def __check_weather_image(self, image_name: str) -> None:
+        """
+        Method which checks if weather icon is already downloaded or download it.
+        If downloading is not success - sets None to __last_icon.
+
+        Args:
+            image_name: Name of weather icon without file type part.
+
+        Returns:
+            None
+        """
         if not self.__weather_image_exists(image_name):
             url = self.__image_url.format(image_name)
             async with aiohttp.ClientSession() as session:
                 async with session.get(url=url) as response:
                     if response.status == 200:
-                        with open(os.path.join(os.getcwd(), "API_Icons", image_name+".png"), "wb") as f:
+                        with open(os.path.join(os.getcwd(), WEATHER_ICONS_FOLDER_NAME, image_name+".png"), "wb") as f:
                             f.write(await response.read())
+                    else:
+                        self.__last_icon = None
         return None
 
     @staticmethod
     def __get_celsius_from_kelvin(t_kelvin: Union[int, float, str]) -> str:
+        """
+        Method which converts temperature from Kelvin scale to Celsius and formats it to the
+        "<sign><temperature> C" like view.
+
+        Args:
+            t_kelvin: String or number with Kelvin scale temperature.
+
+        Returns:
+            String with formatted temperature in Celsius scale.
+
+        """
         t_kelvin = int(t_kelvin)
         t_celsius = t_kelvin-273
         result = u"{} C".format(str(t_celsius))
@@ -69,7 +137,17 @@ class AvatarGenerator(object):
             result = "+" + result
         return result
 
-    def generate(self, avatar_name="Avatar.png"):
+    def generate(self, avatar_name: str = "Avatar.png") -> os.path:
+        """
+        Method which generates avatar image with time and current weather data or only with current time
+        if weather data is not available.
+
+        Args:
+            avatar_name: The name of the file with which the generated avatar will be savedю
+
+        Returns:
+            os.path object - absolute path to the generated avatar image.
+        """
         bg = Image.new("RGBA", (200, 200), self.__bg_color+(255,))
         canvas = ImageDraw.Draw(bg)
         time = "{:0>2d}:{:0>2d}".format(dt.now().hour, dt.now().minute)
